1c9f22277d56519eaa61499222c4d34b77f32103
souheib-riache <souheib.riache@smartinia.com>
2024-07-21 23:41:28 +0200

    complete signup functionality

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,82 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const handleChange = (e: any) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setIsLoading(false);
+      if (data.error) setError(true);
+    } catch (error) {
+      setIsLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign-up</h1>
+      <form action="" className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          id="userName"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          id="confirmPassword"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <button
+          disabled
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase
+        hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? "Loading..." : "Sign up"}
+        </button>
+      </form>
+      <div className="flex gap-2 items-center my-4">
+        <p>Have an account?</p>{" "}
+        <Link to={"/sign-in"}>
+          <span className="text-slate-700 underline">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+    </div>
+  );
 }
